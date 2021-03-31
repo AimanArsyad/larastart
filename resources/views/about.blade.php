d5583e71204af7a438c7603927c8afe76b970580
AimanArsyad <aimanarsyad.isa@gmail.com>
2021-04-01 01:25:50 +0800

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,10 @@
+@extends('layout')
+
+@section('title','About Us')
+
+@section('content') 
 <h1>About Us</h1>
 
 <p>Company Bio</p>
 <p>123-123-123</p>
+@endsection
--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -1,31 +1,80 @@
 @extends('layout')
 
-@section('content')
-<h1>Customers</h1>
+@section('title','Customer list')
 
-<form action="customers" method="POST" class="pb-5">
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <h1>Customers List</h1>    
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12">
+        <form action="customers" method="POST">
 
  @csrf
 
+    
+        <div class="form-group pb-2">
+            <label for="name">Name</label>
+            <input type="text" name="name" value="{{old('name')}}" class="form-control">
+            <div> {{ $errors->first('name')}} </div>  
+        </div>   
+    
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" value="{{old('email')}}" class="form-control">
+            <div> {{ $errors->first('email')}} </div>  
+        </div>  
 
-    <div class="input-group">
-        <input type="text" name="name">
-    </div>   
+        <div class="form-group">
+            <label for="active">Status</label>
+            <select name="active" id="active">
+                <option value="" disabled>Select customer status</option>
+                <option value ="1">Active</option>
+                <option value ="0">Inactive</option>
+            </select>
+        </div> 
     
     
 
-    <button type="submit">Add Customer</button>
+        <button type="submit" class="btn btn-primary">Add Customer</button>
 
  
-</form>    
+        </form>    
 
-{{ $errors->first('name')}}
 
-<ul>
+    </div>
     
-        @foreach ($customers as $customers) 
-                <li>{{  $customers->name }}</li>
+</div>
+
+<hr>
+
+<div class="row">
+    <div class="col-6">
+        <h3>Active Customers</h3>
+        <ul>
+    
+        @foreach ($activeCustomers as $activeCustomers) 
+                <li>{{  $activeCustomers->name }} <span class="text-muted">({{ $activeCustomers->email}})</span></li>
         @endforeach
     
-</ul>
+        </ul>
+    </div>
+
+
+
+    <div class="col-6">
+         <h3>Inactive Customers</h3>
+        <ul>
+    
+        @foreach ($inactiveCustomers as $inactiveCustomers) 
+                <li>{{  $inactiveCustomers->name }} <span class="text-muted">({{ $inactiveCustomers->email}})</span></li>
+        @endforeach
+    
+        </ul>
+    </div>
+</div>
 @endsection
